447e1a0867954cb6d21d056edd814825d55baa2e
FikriAfifK <126756900+FikriAfifK@users.noreply.github.com>
2025-09-04 08:37:21 +0700

    Update VideoPlay.js

--- frontend/src/components/VideoPlay.js
+++ frontend/src/components/VideoPlay.js
@@ -26,25 +26,3 @@ const VideoPlay = () => {
 };
 
 export default VideoPlay;
-
-// import React, { useEffect, useRef } from 'react';
-
-// const VideoPlay = () => {
-//   const videoRef = useRef(null);
-
-//   useEffect(() => {
-//     if (videoRef.current) {
-//       videoRef.current.src = 'http://localhost:5000/start_stream';
-//     }
-//   }, []);
-
-//   return (
-//     <div style={{ padding: '20px' }}>
-//       <div style={{ marginBottom: '20px' }}>
-//         <img ref={videoRef} alt="Video stream" style={{ width: '50%', height: 'auto' }} />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default VideoPlay;
